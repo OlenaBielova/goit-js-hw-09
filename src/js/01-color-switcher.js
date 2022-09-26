e382f5f67fcbd05f1cc9ffd4da26537e7c990558
OlenaBielova <belova.elena11@gmail.com>
2022-09-26 15:19:29 +0300

    1st task

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,34 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+startBtn.disabled = false;
+stopBtn.disabled = true;
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  if (startBtn.disabled) {
+    return;
+  }
+
+  timeID = setInterval(() => {
+    document.body.style.backgroundColor = `${(getRandomHexColor())}`
+  }, 1000);
+
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+
+function onStopBtnClick() {
+  clearInterval(timeID);
+  
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
+
+
